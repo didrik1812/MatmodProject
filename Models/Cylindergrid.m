270d03f0950aa9d10e4d3ca1ceb2c93d690dfe9e
Xavier Raynaud <xavier.raynaud@sintef.no>
2022-11-18 08:02:36 +0100

    added Avogadro constant

--- Models/Cylindergrid.m
+++ Models/Cylindergrid.m
@@ -3,9 +3,9 @@ function G = Cylindergrid(r_max , r_min ,step , z_max, dens)
     arguments
         r_max(1,1) double  = log(0.22*micro*meter);
         r_min(1,1) double  = log(0.1*nano*meter);
-        step(1,1) double  = 0.25;
-        z_max(1,10) double  = convertFrom(ones(10,1)*(15/10), nano*meter);
-        dens(1,1) double  = 20;
+        step(1,1) double   = 0.25;
+        z_max(1,10) double = convertFrom(ones(10,1)*(15/10), nano*meter);
+        dens(1,1) double   = 20;
     end
     P = [];
     for r = exp(r_min:step:r_max)
